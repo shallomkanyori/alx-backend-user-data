55ce049f48accb7dbcd802e4160bb23a23c75eea
Shallom Kanyori <shallomkanyori@gmail.com>
2024-02-07 15:52:26 +0000

    Add ip rather than password to PII_FIELDS

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -15,7 +15,7 @@ import re
 from typing import List
 import logging
 
-PII_FIELDS = ("name", "email", "phone", "ssn", "password")
+PII_FIELDS = ("name", "email", "phone", "ssn", "ip")
 
 
 def filter_datum(fields: List[str], redaction: str, message: str,
