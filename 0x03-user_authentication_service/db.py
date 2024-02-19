ce79e487bd1452742bd8ef0c4a4ec1955d7795c5
Shallom Kanyori <shallomkanyori@gmail.com>
2024-02-19 08:57:50 +0000

    Implement a method to filter users
    Implement a find_user_by method in the DB class that returns the first
    row from the users table as filtered by the keyword arguments.

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -5,6 +5,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
 
 from user import Base, User
 
@@ -37,5 +39,17 @@ class DB:
 
         self._session.add(user)
         self._session.commit()
+
+        return user
+
+    def find_user_by(self, *args, **kwargs) -> User:
+        """Returns first user based on **kwargs"""
+
+        if any(getattr(User, k, None) is None for k in kwargs):
+            raise InvalidRequestError
+
+        user = self._session.query(User).filter_by(**kwargs).first()
+        if user is None:
+            raise NoResultFound
 
         return user
